ab3ad6f6f69773b17cebfceac4ff40c907abe8f6
South <1275093225@qq.com>
2025-09-18 14:58:33 +0800

    feat: 保存权限路由

--- systems/menu/urls.py
+++ systems/menu/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
     path('update/<int:menu_id>', views.update, name='update'),
     path('detail', views.detail, name='detail'),
     path('changeState', views.change_state, name='change_state'),
+    path('tree', views.tree, name='tree'),
+    path('authorize/save', views.save_authorize, name='save_authorize'),
 ]
